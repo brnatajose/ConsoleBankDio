0321fa83e158a0ee9dcf36c44acc8ab43804ee3b
Natã Morais <brnatajose@gmail.com>
2022-03-05 17:25:57 -0400

    Versão 1.0

---
+++ Classes/Conta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForDevs_Fintech
+{
+    public class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+        private string Nome { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Nome = nome;
+            this.Saldo = saldo;
+            this.Credito = credito;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            if (this.Saldo - valorSaque < (this.Credito *-){
+                Console.WriteLine("Saldo insuficiente");
+                return false;
+            }
+        }
+
+    }
+}
